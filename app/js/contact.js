7db1400586caf014f899b7ddb798436294556e87
Ramos <brian.jr.ramos@gmail.com>
2016-06-04 23:24:32 -0700

    final

--- app/js/contact.js
+++ app/js/contact.js
@@ -27,8 +27,6 @@ $(function () {
 			'<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Thank you! We\'ll be in touch!</div>';
 		    $('#contact-form').find('.messages').html(alertBox);
 		    $('#contact-form')[0].reset();
-
-		    console.log(data.toString());
                 }
             });
 
